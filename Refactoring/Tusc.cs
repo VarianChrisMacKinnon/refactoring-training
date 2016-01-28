461b380a009462db28f9696c932f5d4c40a123e2
VarianChrisMacKinnon <christopher.mackinnon@varian.com>
2016-01-28 13:59:27 -0600

    User Searching Refactored. Refactored Magic Numbers

--- Refactoring/Tusc.cs
+++ Refactoring/Tusc.cs
@@ -9,46 +9,43 @@ using System.Threading.Tasks;
 namespace Refactoring
 {
     public class Tusc
-    {
+    {        
+        private static List<User> _userList;
+        private static List<Product> _productList;
+
+        public static int EXIT_NUMBER
+        {
+            get { return _productList.Count + 1; }
+        }
+        public static int ZERO_INDEX_ADJUST
+        {
+            get { return 1; }
+        }
+
         public static void Start(List<User> userList, List<Product> productList)
         {
+            _userList = userList;
+            _productList = productList;
+
             // Write welcome message
             DisplayWelcomeMessage();
 
             // Login
             Login:
-
+            
             string userName = PromptUserForUsername();
 
             // Validate Username
-            bool isValidUser = false;
             if (!string.IsNullOrEmpty(userName))
             {
-                for (int i = 0; i < 5; i++)
-                {
-                    User user = userList[i];
-                    if (user.Name == userName)
-                    {
-                        isValidUser = true;
-                    }
-                }
+                bool isValidUser = ValidateUser(userName);
 
                 if (isValidUser)
                 {
                     string userPassword = PromptUserForPassword();
 
                     // Validate Password
-                    bool isValidPassword = false;
-                    for (int i = 0; i < 5; i++)
-                    {
-                        User user = userList[i];
-
-                        // Check that name and password match
-                        if (user.Name == userName && user.Password == userPassword)
-                        {
-                            isValidPassword = true;
-                        }
-                    }
+                    bool isValidPassword = ValidateUserPassword(userName, userPassword);
 
                     if (isValidPassword)
                     {
@@ -56,44 +53,24 @@ namespace Refactoring
                         DisplayLoginSuccessfulMessage(userName);
                         
                         // Show remaining balance
-                        double userBalance = 0;
-                        for (int i = 0; i < 5; i++)
-                        {
-                            User user = userList[i];
-
-                            // Check that name and password match
-                            if (user.Name == userName && user.Password == userPassword)
-                            {
-                                userBalance = user.Balance;
-
-                                // Show balance 
-                                DisplayCurrentBalance(user.Balance);
-                            }
-                        }
+                        double userBalance = GetCurrentBalance(userName, userPassword);
+                        DisplayCurrentBalance(userBalance);                        
 
                         // Show product list
                         while (true)
                         {
                             // Prompt for user input
-                            DisplayProductList(productList);
+                            DisplayProductList();
 
                             // Prompt for user input
                             string answer = PromptUserForNumber();
                             int productChoice = Convert.ToInt32(answer);
-                            productChoice = productChoice - 1; 
+                            productChoice = productChoice - ZERO_INDEX_ADJUST; 
 
                             // Check if user entered number that equals product count
-                            if (productChoice == 7)
+                            if (productChoice == EXIT_NUMBER)
                             {
-                                // Update balance
-                                foreach (var user in userList)
-                                {
-                                    // Check that name and password match
-                                    if (user.Name == userName && user.Password == userPassword)
-                                    {
-                                        user.Balance = userBalance;
-                                    }
-                                }
+                                UpdateUserBalance(userName, userPassword, userBalance);
 
                                 // Write out new balance
                                 string json = JsonConvert.SerializeObject(userList, Formatting.Indented);
@@ -170,6 +147,84 @@ namespace Refactoring
             PromptUserToExit();
         }
 
+        private static void UpdateUserBalance(string userName, string userPassword, double userBalance)
+        {
+            User user = FindUser(userName, userPassword);
+
+            if( user != null )
+            {
+                user.Balance = userBalance;
+            }
+        }
+
+        private static double GetCurrentBalance(string userName, string userPassword)
+        {
+            double userBalance = 0;
+
+            User user = FindUser(userName, userPassword);
+
+            if( user != null )
+            {
+                userBalance = user.Balance;
+            }
+            
+            return userBalance;
+        }
+
+        private static bool ValidateUserPassword(string userName, string userPassword)
+        {
+            bool isValidPassword = false;
+            
+            if( FindUser(userName, userPassword) != null )
+            {
+                isValidPassword = true;
+            }
+
+            return isValidPassword;
+        }
+
+        private static bool ValidateUser(string userName)
+        {
+            bool isValidUser = false;
+
+            if( FindUser(userName) != null )
+            {
+                isValidUser = true;
+            }
+
+            return isValidUser;
+        }
+
+        private static User FindUser(string userName, string userPassword)
+        {
+            User user = null;
+
+            for (int i = 0; i < _userList.Count; i++)
+            {
+                if (_userList[i].Name == userName && _userList[i].Password == userPassword)
+                {
+                    user = _userList[i];
+                }
+            }
+
+            return user;
+        }
+
+        private static User FindUser(string userName)
+        {
+            User user = null;
+
+            for (int i = 0; i < _userList.Count; i++)
+            {
+                if (_userList[i].Name == userName)
+                {
+                    user = _userList[i];
+                }
+            }
+
+            return user;
+        }
+
         #region Display Messages
 
         private static void DisplayPurchasePlan(double userBalance,string productName)
@@ -179,16 +234,16 @@ namespace Refactoring
             Console.WriteLine("Your balance is " + userBalance.ToString("C"));
         }
 
-        private static void DisplayProductList(List<Product> productList)
+        private static void DisplayProductList()
         {
             Console.WriteLine();
             Console.WriteLine("What would you like to buy?");
-            for (int i = 0; i < 7; i++)
+            for (int i = 0; i < _productList.Count; i++)
             {
-                Product product = productList[i];
-                Console.WriteLine(i + 1 + ": " + product.Name + " (" + product.Price.ToString("C") + ")");
+                Product product = _productList[i];
+                Console.WriteLine(i + ZERO_INDEX_ADJUST + ": " + product.Name + " (" + product.Price.ToString("C") + ")");
             }
-            Console.WriteLine(productList.Count + 1 + ": Exit");
+            Console.WriteLine(EXIT_NUMBER + ": Exit");
         }
 
         private static void DisplayPurchaseSummary(string productName, double userBalance, int purchaseQuantity)
